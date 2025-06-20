11468df9fbf2564bd480ed1504fb67c6de80843e
Yu-Chen Chen <yuchen@Yu-Chens-Laptop.local>
2025-06-20 16:51:12 -0700

    Modify next.config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,5 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-const { withNetlify } = require('@netlify/next');
-module.exports = withNetlify({});
 
 export default nextConfig;
